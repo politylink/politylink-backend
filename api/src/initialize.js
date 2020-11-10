01e023177e282544ad0225ccb56d26eb214edf6a
6br <tfdvrt@gmail.com>
2020-11-10 16:34:37 +0900

    Add async/await for initialization

--- api/src/initialize.js
+++ api/src/initialize.js
@@ -2,7 +2,7 @@ export const initializeDatabase = (driver) => {
   const resources = ["Member", "Election", "Diet", "Law", "Bill", "Committee", "Minutes", "Url", "Timeline", "News"]
   const initCypher = resources.map(key => `CREATE CONSTRAINT ON (n:${key}) ASSERT n.id IS UNIQUE`)
 
-  const executeQuery = (driver, cypher) => {
+  const executeQuery = async(driver, cypher) => {
     const session = driver.session()
     return session
       .writeTransaction((tx) => tx.run(cypher))
@@ -10,8 +10,8 @@ export const initializeDatabase = (driver) => {
       .finally(() => session.close())
   }
 
-  initCypher.forEach(cypher => 
-    executeQuery(driver, cypher).catch((error) => {
+  initCypher.forEach(async(cypher) => 
+    await executeQuery(driver, cypher).catch((error) => {
       console.error('Database initialization failed to complete\n', error.message)
     })
   )
